fef7ab8ef4610730620622a70a71afa081fb874a
Jay <jaykhurana16777@gmail.com>
2024-03-15 10:56:39 +0530

    .

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,25 +1,10 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> {
-  const { kindeAuth } = req.query;
-
-  if (!kindeAuth || typeof kindeAuth !== 'string') {
-    res.status(400).json({ error: 'Invalid request' });
-    return;
-  }
-
-  try {
-    // Call the handleAuth function
-    await handleAuth(req, kindeAuth);
-
-    // Send a success response
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error('Error handling auth:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+export async function GET(
+  request: NextRequest,
+  { params }: any
+) {
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
 }
