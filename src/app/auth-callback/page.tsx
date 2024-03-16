e11a4a6d45c3272aeb4d15ff935eb1f5c8050150
Jay <jaykhurana16777@gmail.com>
2024-03-16 11:44:40 +0530

    .

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,46 +1,46 @@
 "use client"
-
-import { useRouter, useSearchParams } from "next/navigation"
-import { trpc } from "../_trpc/client"
-import { Loader2 } from "lucide-react"
+import { useRouter } from "next/router";
+import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
+import ErrorPage from 'next/error';
 
 const Page = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const searchParams = useSearchParams()
-  const origin = searchParams.get('origins')
+  const isFallback = 'isFallback' in router ? router.isFallback : false;
+  const { post, origins } = router.query;
+
+  if (!isFallback && !post) {
+    return <ErrorPage statusCode={404} />;
+  }
 
   const query = trpc.authCallback.useQuery(undefined, {
     retry: true,
     retryDelay: 500,
   });
 
-  // Check for errors in the query result
   if (query.error) {
     const errData = query.error.data;
     if (errData?.code === 'UNAUTHORIZED') {
       router.push('/sign-in');
     } else {
-      // Handle other types of errors
       console.error("An error occurred:", query.error);
     }
   }
 
-  // Continue with other logic based on the query result
   if (query.data?.success) {
-    router.push(origin ? `/${origin}` : '/dashboard');
+    router.push(origins ? `/${origins}` : '/dashboard');
   }
 
   return (
     <div className="w-full mt-24 flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-            <Loader2  className="h-8 w-8 animate-spin text-zinc-800"/>
-            <h3 className="font-semibold text-xl">Setting up your account...</h3>
-            <p>You will be redirected automatically.</p>
-        </div>
+      <div className="flex flex-col items-center gap-2">
+        <Loader2  className="h-8 w-8 animate-spin text-zinc-800"/>
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Page
+export default Page;
