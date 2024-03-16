556d7822eaf6ac81fcfb5902b1ecfcfdb16a7ae8
Jay <jaykhurana16777@gmail.com>
2024-03-16 12:29:35 +0530

    .

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,22 +1,27 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { TRPCError, initTRPC } from '@trpc/server'
 
-const t = initTRPC.create();
+const t = initTRPC.create()
+const middleware = t.middleware
 
-const isAuth = async (opts: { ctx: any }): Promise<any> => {
-    const { getUser } = getKindeServerSession();
-    const user: any = await getUser();
+const isAuth = middleware(async(opts)=> {
 
-    if (!user || !user.id) {
-        throw new TRPCError({ code: 'UNAUTHORIZED' });
+    const {getUser} = getKindeServerSession()
+    const user = await getUser()
+
+    if(!user || !user.id){
+        throw new TRPCError({ code: 'UNAUTHORIZED'})
     }
 
-    return {
-        userId: user.id,
-        user,
-    };
-};
+    return opts.next({
+        ctx: {
+            userId: user.id,
+            user,
+        }
+    })
+
+})
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
-export const privateProcedure = t.procedure.use(isAuth);
+export const privateProcedure = t.procedure.use(isAuth)
